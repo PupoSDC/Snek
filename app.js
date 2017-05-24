5eb8416b7460b2d08605d88701b7e70b20b9a786
pupo162 <pupo163@hotmail.com>
2017-05-24 22:43:11 +0200

    added snake movement to client and server

--- app.js
+++ app.js
@@ -34,13 +34,12 @@ var board = {
   board_x:     40,
   board_y:     40, 
   snakes:      [],
-  my_snake:    null,
   food:        [],
   create_food: function(){
-    if( food.length < 5 ){
+    if( this.food.length < 5 ){
       this.food.push({
-        x: Math.round(Math.random()*board_x), 
-        y: Math.round(Math.random()*board_y), 
+        x: Math.round(Math.random()*this.board_x), 
+        y: Math.round(Math.random()*this.board_y), 
       });
     }
   },
@@ -50,98 +49,65 @@ var board = {
       direction: "right",
       colour:    "red",
     }
-    for( var i = 0;  i < 5; i++ ){
+    for( var i = 4;  i >= 0; i-- ){
       snake.cells.push({x: i, y:0});
     }
-    this.snakes.push(snake);
+    return snake;
   },
   update: function(){
+    for( var i = 0; i < this.snakes.length; i++ ){
+      var    snake = this.snakes[i];
+      var     head = snake.cells[ snake.cells.length - 1 ];
+      var new_head = {x: head.x, y: head.y };
 
-    //The movement code for the snake to come here.
-    //The logic is simple
-    //Pop out the tail cell and place it infront of the head cell
-    //var nx = snake_array[0].x;
-    //var ny = snake_array[0].y;
-    //These were the position of the head cell.
-    //We will increment it to get the new head position
-    //Lets add proper direction based movement now
-    //if(d == "right") nx++;
-    //else if(d == "left") nx--;
-    //else if(d == "up") ny--;
-    //else if(d == "down") ny++;
-    
-    //Lets add the game over clauses now
-    //This will restart the game if the snake hits the wall
-    //Lets add the code for body collision
-    //Now if the head of the snake bumps into its body, the game will restart
-    //if(nx == -1 || nx == w/cw || ny == -1 || ny == h/cw || check_collision(nx, ny, snake_array))
-    //{
-    //  //restart game
-    //  //init();
-    //  //Lets organize the code a bit now.
-    //  return;
-    //}
-    
-    //Lets write the code to make the snake eat the food
-    //The logic is simple
-    //If the new head position matches with that of the food,
-    //Create a new head instead of moving the tail
-    //if(nx == food.x && ny == food.y){
-    //  var tail = {x: nx, y: ny};
-    //  score++;
-    //  //Create new food
-    //  create_food();
-    //}
-    //else
-    //{
-    //  var tail = snake_array.pop(); //pops out the last cell
-    //  tail.x = nx; tail.y = ny;
-    //}
-    //The snake can now eat the food.
-    
-    //snake_array.unshift(tail); //puts back the tail as the first cell
-    
-    //for(var i = 0; i < snake_array.length; i++)
-    //{
-    //  var c = snake_array[i];
-    //  //Lets paint 10px wide cells
-    //  paint_cell(c.x, c.y);
-    //}
-    
-    //Lets paint the food
-    
-    //Lets paint the score
-    //var score_text = "Score: " + score;
-    //canvas.fillText(score_text, 5, h-5);
+           if(snake.direction == "right"){ new_head.x++; }
+      else if(snake.direction == "left" ){ new_head.x--; }
+      else if(snake.direction == "up"   ){ new_head.y++; }
+      else if(snake.direction == "down" ){ new_head.y--; }
+      snake.cells.push(new_head); 
+
+      if( new_head.x >= this.board_x || new_head.x < 0 ||   
+          new_head.y >= this.board_y || new_head.y < 0 ){
+        this.snakes[i] = this.create_snake();
+      }
+    }
+
+
   },
   game_loop: function(){
     var t = this;
-    setInterval(function(){t.update();}, 100);
+    setInterval(function(){t.update();}, 60);
+    setInterval(function(){t.create_food();}, 1000);
   }
 }
 
+board.game_loop();
 
 wss.on('connection', function(ws){
-  board.create_snake();
-  ws.snake = board.snakes[board.snakes.length];
+  ws.snake = board.create_snake();  
+  board.snakes.push(ws.snake);
+  
+
+  var update_client = setInterval(function(){
+    var message = JSON.stringify({ snakes: board.snakes, food: board.food });
+    ws.send(message);
+  }, 60);
 
   ws.on('close', function () {
     board.snakes.splice(board.snakes.indexOf(ws.snake),1);
+    clearInterval(update_client);
   });
   ws.on('message', function(message){
-    if( ws.snake.direction == "right" && message != "left" ){
-      ws.snake.direction = "right";
-    }
-    else if( ws.snake.direction == "left" && message != "right" ){
-      ws.snake.direction = "left";
-    }
-    else if( ws.snake.direction == "up" && message != "down" ){
-      ws.snake.direction = "up";
-    }
-    else if( ws.snake.direction == "down" && message != "up" ){
-      ws.snake.direction = "down";
-    }
+      if( message == "left"  && ws.snake.direction == "right"){ return; }
+      if( message == "right" && ws.snake.direction == "left" ){ return; }
+      if( message == "down"  && ws.snake.direction == "up"   ){ return; }
+      if( message == "up"    && ws.snake.direction == "down" ){ return; }
+      ws.snake.direction = message;
   });
+
+
+
+
 });
 
 
--- public/js.js
+++ public/js.js
@@ -13,26 +13,25 @@ function handleResponse(response){
 	  : Promise.reject(new Error('Unexpected response'));
 };
 
-ws.onmessage = function(event){ showMessage(event.data);                    };
 ws.onerror   = function(){ showMessage('WebSocket error');                  }; 
 ws.onopen    = function(){ showMessage('WebSocket connection established'); };
 ws.onclose   = function(){ showMessage('WebSocket connection closed');      };
 
 var board = {
-	area:        document.querySelector('#canvas').getContext("2d"),
-	cell_width:  10,
-	snakes:      [],
-	my_snake:    null,
-	food:        [],
-	width:       document.querySelector('#canvas').width,
-	height:      document.querySelector('#canvas').height,
+	area:          document.querySelector('#canvas').getContext("2d"),
+	snakes:        [],
+	food:          [],
+	canvas_width:  document.querySelector('#canvas').width,
+	canvas_height: document.querySelector('#canvas').height,
+	width:         40,
+	height:        40,
 
 	update: function(){
 
 		this.area.fillStyle   = "white";
 		this.area.strokeStyle = "black";
-		this.area.fillRect(0, 0,   this.width, this.height);
-		this.area.strokeRect(0, 0, this.width, this.height);
+		this.area.fillRect(0, 0,   this.canvas_width, this.canvas_height);
+		this.area.strokeRect(0, 0, this.canvas_width, this.canvas_height);
 		
 		for( var i = 0; i < this.food.length; i++ ){
 			this.paint_food( this.food[i] );
@@ -41,77 +40,21 @@ var board = {
 		for( var i = 0; i < this.snakes.length; i++ ){
 			this.paint_snake( this.snakes[i] );
 		}
-		//The movement code for the snake to come here.
-		//The logic is simple
-		//Pop out the tail cell and place it infront of the head cell
-		//var nx = snake_array[0].x;
-		//var ny = snake_array[0].y;
-		//These were the position of the head cell.
-		//We will increment it to get the new head position
-		//Lets add proper direction based movement now
-		//if(d == "right") nx++;
-		//else if(d == "left") nx--;
-		//else if(d == "up") ny--;
-		//else if(d == "down") ny++;
-		
-		//Lets add the game over clauses now
-		//This will restart the game if the snake hits the wall
-		//Lets add the code for body collision
-		//Now if the head of the snake bumps into its body, the game will restart
-		//if(nx == -1 || nx == w/cw || ny == -1 || ny == h/cw || check_collision(nx, ny, snake_array))
-		//{
-		//	//restart game
-		//	//init();
-		//	//Lets organize the code a bit now.
-		//	return;
-		//}
-		
-		//Lets write the code to make the snake eat the food
-		//The logic is simple
-		//If the new head position matches with that of the food,
-		//Create a new head instead of moving the tail
-		//if(nx == food.x && ny == food.y){
-		//	var tail = {x: nx, y: ny};
-		//	score++;
-		//	//Create new food
-		//	create_food();
-		//}
-		//else
-		//{
-		//	var tail = snake_array.pop(); //pops out the last cell
-		//	tail.x = nx; tail.y = ny;
-		//}
-		//The snake can now eat the food.
-		
-		//snake_array.unshift(tail); //puts back the tail as the first cell
-		
-		//for(var i = 0; i < snake_array.length; i++)
-		//{
-		//	var c = snake_array[i];
-		//	//Lets paint 10px wide cells
-		//	paint_cell(c.x, c.y);
-		//}
-		
-		//Lets paint the food
-		
-		//Lets paint the score
-		//var score_text = "Score: " + score;
-		//canvas.fillText(score_text, 5, h-5);
 	},
 	paint_food: function(food){
 		this.area.fillStyle   = "blue";
 		this.area.strokeStyle = "white";
 		this.area.fillRect( 
-			food.x*this.cell_width, 
-			food.y*this.cell_width, 
-			this.cell_width, 
-			this.cell_width
+			food.x*this.canvas_width  / this.width , 
+			food.y*this.canvas_width  / this.width , 
+			this.canvas_width  / this.width,
+			this.canvas_height / this.height
 		);
 		this.area.strokeRect( 
-			food.x*this.cell_width, 
-			food.y*this.cell_width, 
-			this.cell_width, 
-			this.cell_width
+			food.x*this.canvas_width  / this.width , 
+			food.y*this.canvas_height / this.height, 
+			this.canvas_width  / this.width,
+			this.canvas_height / this.height
 		);
 	},
 	paint_snake: function(snake){
@@ -119,16 +62,16 @@ var board = {
 		this.area.strokeStyle = "white";
 		for( var i = 0; i < snake.cells.length; i++ ){
 			this.area.fillRect( 
-				snake.cells[i].x * this.cell_width, 
-				snake.cells[i].y * this.cell_width, 
-				this.cell_width, 
-				this.cell_width
+				snake.cells[i].x * this.canvas_width  / this.width ,  
+				snake.cells[i].y * this.canvas_width  / this.width , 
+				this.canvas_width  / this.width,
+				this.canvas_width  / this.width
 			);
 			this.area.strokeRect( 
-				snake.cells[i].x * this.cell_width, 
-				snake.cells[i].y * this.cell_width, 
-				this.cell_width, 
-				this.cell_width
+				snake.cells[i].x * this.canvas_width  / this.width ,  
+				snake.cells[i].y * this.canvas_width  / this.width , 
+				this.canvas_width  / this.width,
+				this.canvas_width  / this.width
 			);			
 		}
 
@@ -136,7 +79,6 @@ var board = {
 	game_loop: function(){
 		var t = this;
  		setInterval(function(){t.update();}, 100);
- 		setInterval(function(){t.create_food();}, 1000);
 	}
 }
 
@@ -152,23 +94,10 @@ document.addEventListener("keydown",function(e){
 	}	
 });
 
-
-/*
-//Lets paint the snake now
-function 
-
-//Lets first create a generic function to paint cells
-
-
-function check_collision(x, y, array){
-	//This function will check if the provided x/y coordinates exist
-	//in an array of cells or not
-	for(var i = 0; i < array.length; i++)
-	{
-		if(array[i].x == x && array[i].y == y)
-		 return true;
-	}
-	return false;
-}
-
-*/
+ws.onmessage = function(event){ 
+	var obj = JSON.parse(event.data);
+	board.snakes = obj.snakes;
+	board.food   = obj.food;
+	board.update();
+	console.log(event.data)                   
+};
