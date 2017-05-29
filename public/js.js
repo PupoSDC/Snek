bc968689d2d74674acb73c331b07de02770b29ac
pupo162 <pupo163@hotmail.com>
2017-05-29 22:28:54 +0200

    added touch screen support

--- public/js.js
+++ public/js.js
@@ -74,19 +74,9 @@ var board = {
 	}
 }
 
-window.onresize = function(event) { board.resize(); };
-
-document.addEventListener("keydown",function(e){
-	if(ws) {
-		var key = e.which;
-		     if(key == "37"){ ws.send("left");  }
-		else if(key == "38"){ ws.send("up");    }  
-		else if(key == "39"){ ws.send("right"); }
-		else if(key == "40"){ ws.send("down");	}
-	}	
-});
-
 var ws = new WebSocket(`ws://${location.host}`);
+
+window.onresize = function(event) { board.resize(); };
 
 ws.onerror   = function(){ console.log('WebSocket error');                  }; 
 ws.onopen    = function(){ console.log('WebSocket connection established'); };
@@ -102,38 +92,27 @@ ws.onmessage = function(event){
 board.resize();
 board.game_loop();
 
-// Set up touch events for mobile, etc
-document.querySelector('#canvas').addEventListener("touchstart", function(e){
-    mousePos = getTouchPos(canvas, e);
-	var touch      = e.touches[0];
-	var mouseEvent = new MouseEvent("mousedown", {
-	clientX: touch.clientX,
-	clientY: touch.clientY
-	});
-	canvas.dispatchEvent(mouseEvent);
+// control snake with arrow keys
+document.addEventListener("keydown",function(e){
+	if(ws) {
+		var key = e.which;
+		     if(key == "37"){ ws.send("left");  }
+		else if(key == "38"){ ws.send("up");    }  
+		else if(key == "39"){ ws.send("right"); }
+		else if(key == "40"){ ws.send("down");	}
+	}	
+});
+
+// control snake with touch screen
+document.querySelector('#canvas').addEventListener("touchend", function(e) {
+	var rect = document.querySelector('#canvas').getBoundingClientRect()
+	var pos = { x: e.changedTouches[0].clientX - rect.left,
+	   			y: e.changedTouches[0].clientY - rect.top };
+
+		 if( pos.y <   rect.height / 4 ){ ws.send("up");    }
+	else if( pos.y > 3*rect.height / 4 ){ ws.send("down");  }
+	else if( pos.x <   rect.width  / 2 ){ ws.send("left");  }
+	else if( pos.x >   rect.width  / 2 ){ ws.send("right"); }
+
+	
 }, false);
-
-
-document.querySelector('#canvas').addEventListener("touchend", function (e) {
-	var pos = getTouchPos( document.querySelector('#canvas'), e);
-	console.log(pos);
-}, false);
-
-
-canvas.addEventListener("touchmove", function (e) {
-  var touch = e.touches[0];
-  var mouseEvent = new MouseEvent("mousemove", {
-    clientX: touch.clientX,
-    clientY: touch.clientY
-  });
-  canvas.dispatchEvent(mouseEvent);
-}, false);
-
-// Get the position of a touch relative to the canvas
-function getTouchPos(canvasDom, touchEvent) {
-  var rect = canvasDom.getBoundingClientRect();
-  return {
-    x: touchEvent.touches[0].clientX - rect.left,
-    y: touchEvent.touches[0].clientY - rect.top
-  };
-}
